02fda7d8fba5221d0b1c21fa357da4942d9ef979
As Salih <thickino@gmail.com>
2023-07-16 12:42:43 +0000

    création d'un composant products dans => src

--- jsxapp/src/App.js
+++ jsxapp/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Products from './Products';
+
 
 const App = () => {
   return (
     <div>
-      <h1>
-        hello react
-      </h1>
+      <h1>hello react </h1>
+      <Products nama={"ballo"} price={25} description={"Grosse balle à jouer, le plus souvent ronde, généralement formée d'une vessie gonflée d'air et recouverte d'une peau cousue, utilisée dans les sports d'équipe comme le football, le rugby, le basket-ball, le handball, le volley-ball."} img={"https://unsplash.com/fr/photos/LTyPTQ2tgNA"} />
     </div>
   );
 }
---
+++ jsxapp/src/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Products = (props) => {
+    console.log(props)
+    return(
+    <>
+        <h1> Ballon </h1>
+    </>
+    )
+}
+
+export default Products;
